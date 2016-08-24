3b60772270bb6c425dfe7b097996d9d64b2480c9
Jack <xszaboj@gmail.com>
2016-08-24 15:17:58 +0100

    add trash

--- componentHandlers.js
+++ componentHandlers.js
@@ -1,3 +1,8 @@
 $("#add").click(function(){
     addHex();
 });
+
+$("#add-path").click(function(){
+    var type = getPathType();
+    createLine(type);
+});
--- manipulate.js
+++ manipulate.js
@@ -26,3 +26,26 @@ function moveSelectedElementOnTop(){
         this.parentElement.appendChild(this);
    };
 }
+
+function isInTrash(element){
+    var trash = document.getElementById("trash");
+    if(intersectRect(trash, element)){
+        return true;
+    }
+    return false;
+}
+
+function intersectRect(r1, r2) {
+    var r1 = r1.getBoundingClientRect();    //BOUNDING BOX OF THE FIRST OBJECT
+    var r2 = r2.getBoundingClientRect();    //BOUNDING BOX OF THE SECOND OBJECT
+ 
+    //CHECK IF THE TWO BOUNDING BOXES OVERLAP
+  return !(r2.left > r1.right || 
+           r2.right < r1.left || 
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top);
+}
+
+function removeElement(element){
+    element.parentNode.removeChild(element);
+}
